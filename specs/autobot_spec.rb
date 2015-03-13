683b18e1bfad3085d56886207fd5963ab45c66bd
Sergio G <sergiorodriguezgijon@gmail.com>
2015-03-14 00:56:58 +0100

    Removin' warnings. Added a new it block

--- specs/autobot_spec.rb
+++ specs/autobot_spec.rb
@@ -4,19 +4,19 @@ describe Autobot do
   let(:optimus) { Autobot.new(name: 'Optimus Prime', age:9999, max_passengers: 3, manufacturer: 'The Spark', model_num: 'AUTB001-001-001', wheelnum: 602, max_ammo: 3) }
 
   it 'is strapped' do
-    expect(optimus.weapons?).to be_true
+    expect(optimus.weapons?).to be_truthy
 
     expect(optimus.remaining_ammo).to eq(3)
-    expect(optimus.shoot!).to be_true
+    expect(optimus.shoot!).to be_truthy
 
     expect(optimus.remaining_ammo).to eq(2)
-    expect(optimus.shoot!).to be_true
+    expect(optimus.shoot!).to be_truthy
 
     expect(optimus.remaining_ammo).to eq(1)
-    expect(optimus.shoot!).to be_true
+    expect(optimus.shoot!).to be_truthy
 
     expect(optimus.remaining_ammo).to eq(0)
-    expect(optimus.shoot!).to be_false
+    expect(optimus.shoot!).to be_falsey
 
     expect(optimus.reload!)
     expect(optimus.remaining_ammo).to eq(3)
@@ -29,10 +29,15 @@ describe Autobot do
   end
 
   it 'is electric' do
-    expect(optimus.electric?).to be_true
+    expect(optimus.electric?).to be_truthy
   end
 
   it 'is damn old' do
     expect(optimus.age).to eq(9999)
   end
+
+  it 'has a name' do
+    expect(optimus.name).to eq('Optimus Prime')
+  end
+  
 end
